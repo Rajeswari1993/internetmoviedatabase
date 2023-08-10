17d998ff157a276c4ae4bf65fc9c5b9bc7d63c0e
Rajeswari1993 <rajeswaricse@outlook.com>
2023-08-10 08:49:56 +0530

    internet movie database application

---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+function NotFound() {
+    return (
+        <section>
+            <h2>Oops!! Page Not Found</h2>
+            <h4>You seem to have lost your way</h4>
+        </section>
+    )
+}
+
+export default NotFound;
